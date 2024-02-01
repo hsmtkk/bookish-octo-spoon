a44d1ed26a13d842eb38d7f1ed2655cdb1bf2578
hsmtkk <hsmtkk@gmail.com>
2024-02-01 18:55:13 +0900

    add icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import Layout from "@/components/layout"
 import "./globals.css";
 
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
 export default function RootLayout({
   children,
 }: {
